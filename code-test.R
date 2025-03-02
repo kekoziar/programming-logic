f0b65fd04d04244d0ab3280d980a858b5037637b
KE Koziar <kkoziar@mail.fresnostate.edu>
2025-03-02 15:02:40 -0800

    conditionals, we might need to rework the second half on syntax. is it too much?

--- code-test.R
+++ code-test.R
@@ -6,3 +6,15 @@ for (i in 1:5){
 }# end i for loop
 
 print(matrix_demo[,1])
+
+
+x = 1
+y = 2
+
+if (x == y) {
+  print("values are equal");
+} else if (x > y) {
+  print("x greater than y");
+} else {
+  print("x must be less than y");
+}
